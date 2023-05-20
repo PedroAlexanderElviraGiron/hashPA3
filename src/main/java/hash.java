ef6313e41c2f20998a3576cb5c67420dd873d940
PedroAlexanderElviraGiron <99924227+PedroAlexanderElviraGiron@users.noreply.github.com>
2023-05-20 08:58:09 -0600

    Add files via upload

---
+++ src/main/java/hash.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Pedro
+ */
+public class hash {
+    //Crear una clase
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  hashMethod(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const address = this.hashMethod(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("Maria", 1979);
+}
